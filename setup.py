c81e3fe3b4ea864983cc2b290960e53f8cac7ba1
ltetrel <loic.tetrel.pro@gmail.com>
2019-07-03 10:45:27 -0400

    changed version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='data2bids',
-      version='1.1',
+      version='1.2',
       description='Reorganize fmri files to make them BIDS compliant.',
       url='https://github.com/SIMEXP/Data2Bids',
       author='Loic TETREL',
